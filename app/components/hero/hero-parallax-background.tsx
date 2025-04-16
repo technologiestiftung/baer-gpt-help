f201e18d2cd0960a951075d66dfff06621e57ba3
aeschi <anna.eschenbacher@ts.berlin>
2025-04-16 10:25:40 +0200

    fix: overflowing icons

--- app/components/hero/hero-parallax-background.tsx
+++ app/components/hero/hero-parallax-background.tsx
@@ -3,21 +3,27 @@ import { Parallax } from "react-scroll-parallax";
 export function HeroParallaxBackground() {
 	return (
 		<>
-			<Parallax speed={-30} className="absolute w-full h-full">
+			<Parallax speed={-30} className="absolute w-full h-full overflow-hidden">
 				<img
 					src="/icons/light-bulb.svg"
 					alt="icon light-bulb"
 					className="w-[320px] top-0 right-0 lg:w-[440px] lg:-top-20 lg:right-20 rotate-12 absolute fast"
 				/>
 			</Parallax>
-			<Parallax speed={-70} className="hidden lg:flex absolute w-full h-full">
+			<Parallax
+				speed={-70}
+				className="hidden lg:flex absolute w-full h-full overflow-hidden"
+			>
 				<img
 					src="/icons/question.svg"
 					alt="icon question"
 					className="top-[370px] w-[300px] -left-20 lg:top-130 lg:left-10 -rotate-12 absolute"
 				/>
 			</Parallax>
-			<Parallax speed={-40} className="hidden lg:flex absolute w-full h-full">
+			<Parallax
+				speed={-40}
+				className="hidden lg:flex absolute w-full h-full overflow-hidden"
+			>
 				<img
 					src="/icons/magnifying-glass.svg"
 					alt="icon magnifying-glass"
