644f3113d743c6be61b1d6a60eb2e02fbeb86325
Zainab Tariq <zainabtariq12@gmail.com>
2025-04-16 14:32:03 +0200

    feat: cleanup (#16)
    * feat: remove unused buttons
    
    * feat: increase z-index for faq

--- app/components/top-faq/top-faq.tsx
+++ app/components/top-faq/top-faq.tsx
@@ -5,7 +5,7 @@ import { content } from "../../content";
 
 export const TopFaq = () => {
 	return (
-		<div className="flex flex-col items-center justify-center gap-6 md:gap-8 px-5 py-[60px] md:py-20">
+		<div className="relative z-10 flex flex-col items-center justify-center gap-6 md:gap-8 px-5 py-[60px] md:py-20">
 			<div className="flex flex-col gap-6 md:gap-5 lg:gap-12 w-full lg:max-w-[942px]">
 				<h2 className="text-4xl leading-10 font-semibold text-center text-dunkelblau-200">
 					{content["faq.title"]}
