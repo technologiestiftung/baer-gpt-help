13121ee97d95c7c7fdac82ae6740e52b83b51c5e
Zainab Tariq <zainabtariq12@gmail.com>
2025-04-23 11:54:15 +0200

    fix: typos and styles (#19)

--- app/components/accordion-content-elements/download-item.tsx
+++ app/components/accordion-content-elements/download-item.tsx
@@ -14,7 +14,7 @@ export const DownloadItem: React.FC<DownloadItemProps> = ({
 	href,
 }) => {
 	return (
-		<div className="flex flex-col lg:flex-row gap-3 lg:gap-0 justify-between px-1 lg:px-3 py-3 border-b-[0.5px] border-dunkelblau-200">
+		<div className="flex flex-col lg:flex-row gap-3 lg:gap-0 justify-between px-0 lg:px-3 py-3 border-b-[0.5px] border-dunkelblau-200">
 			<div className="flex flex-col">
 				<p className="text-base leading-6 font-semibold">{title}</p>
 				<p className="text-xs leading-4 font-normal">{subtitle}</p>
