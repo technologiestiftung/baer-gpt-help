f353ecf8d3961e3a66cedaa2d31f29a7432a48e2
aeschi <56318362+aeschi@users.noreply.github.com>
2025-11-04 16:59:56 +0100

    feat: rename from prompts to working with baergpt

--- app/routes/working-with-baergpt/index.tsx
+++ app/routes/working-with-baergpt/index.tsx
@@ -1,15 +1,15 @@
 import { Accordion } from "../../components/primitives/accordion/accordion";
 import { content } from "../../content";
-import { prompts } from "./prompts-content";
+import { accordionContent } from "./working-with-baergpt-content";
 
 export default function Index() {
 	return (
 		<div className="flex flex-col justify-center mx-auto mb-[60px] lg:mb-[100px] px-5 lg:px-[50px] max-w-[1412px]">
 			<h1 className="py-10 md:py-[60px] lg:py-20 text-4xl leading-10 font-semibold md:text-6xl md:leading-none">
-				{content["prompts.title"]}
+				{content["workingWithBearGPT.title"]}
 			</h1>
 			<div className="flex flex-col gap-10 lg:gap-16">
-				{prompts.sections.map((section, index) => (
+				{accordionContent.sections.map((section, index) => (
 					<div key={index}>
 						<h2 className="mb-5 md:mb-6 text-xl leading-7 font-semibold md:text-3xl md:leading-9 lg:text-4xl lg:leading-10">
 							{section.title}
