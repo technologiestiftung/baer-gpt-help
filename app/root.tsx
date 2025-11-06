dddcfeb28a526f34b2138a0a14241b41592abfdc
aeschi <56318362+aeschi@users.noreply.github.com>
2025-11-06 10:34:34 +0100

    feat: add cookie banner implementation (#36)

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,7 @@ import "./app.css";
 import { Header } from "~/components/header/header";
 import { Footer } from "~/components/footer/footer";
 import { useMenuStore } from "./store/use-menu-store";
+import { CookieBanner } from "~/components/cookie-banner/cookie-banner";
 
 export const links: Route.LinksFunction = () => [
 	{ rel: "icon", type: "image/svg+xml", href: "/citylab.png" },
@@ -55,6 +56,7 @@ export function Layout({ children }: { children: ReactNode }) {
 				<div className="flex z-100 h-[50px] lg:h-[80px] w-full" />
 				<main>{children}</main>
 				<Footer />
+				<CookieBanner />
 				<ScrollRestoration />
 				<Scripts />
 			</body>
