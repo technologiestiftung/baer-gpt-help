b5ee5f1202b3f316598c63722758edc7609d76b1
Zainab Tariq <zainabtariq12@gmail.com>
2026-05-05 15:11:57 +0200

    feat: implement VideoDetail component

--- app/routes/tutorials/video-detail/index.tsx
+++ app/routes/tutorials/video-detail/index.tsx
@@ -1,9 +1,5 @@
-import { useParams } from "react-router";
 import { content } from "~/content";
-import { Feedback } from "~/components/feedback/feedback";
-import { Link } from "react-router";
-import { createSlug } from "~/utils/create-slug";
-import { VimeoPlayer } from "~/components/primitives/video-players/vimeo-player";
+import { VideoDetail } from "~/components/primitives/video-detail/video-detail";
 
 const tutorialVideoElements = content["tutorials.sections"].flatMap((section) =>
 	section.videos.map((video) => ({
@@ -12,39 +8,10 @@ const tutorialVideoElements = content["tutorials.sections"].flatMap((section) =>
 	})),
 );
 
-export function VideoDetail() {
-	const { "video-slug": slug } = useParams();
-
-	const videoElement = tutorialVideoElements.find(
-		(video) => createSlug(video.videoTitle) === slug,
-	);
-
+export function TutorialsVideoDetail() {
 	return (
-		<>
-			<div className="2xl:max-w-[1174px] xl:max-w-[964px] mx-auto flex flex-col xl:pt-6 p-5">
-				<div className="text-base text-dunkelblau-200 flex flex-row gap-1 items-baseline md:items-end">
-					<Link
-						to={`/tutorials/`}
-						className="flex-shrink-0 text-sm lg:text-base hover:underline hover:underline-offset-6"
-					>
-						{content["tutorials.title"]}
-					</Link>
-					<span className="text-base">/</span>
-					<div className="font-bold text-sm lg:text-base">
-						{videoElement?.videoTitle}
-					</div>
-				</div>
-				<h1 className="text-lg leading-7 lg:text-5xl lg:leading-none font-bold mt-10 md:mt-[60px] mb-3 lg:mt-20 lg:mb-7">
-					{videoElement?.videoTitle}
-				</h1>
-				<VimeoPlayer
-					srcUrl={`${videoElement?.videoLink}&color=1E3791&title=0&byline=0&portrait=0&texttrack=de`}
-					title={`Video: ${videoElement?.videoTitle}`}
-				/>
-			</div>
-			<Feedback />
-		</>
+		<VideoDetail mainRoute="tutorials" videoElements={tutorialVideoElements} />
 	);
 }
 
-export default VideoDetail;
+export default TutorialsVideoDetail;
